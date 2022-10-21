dec966c791f675c3481f0ff2f266c255e42a3e6d
zanrizkir <sauzan0547@gmail.com>
2022-10-21 10:38:18 +0700

    Merubah

--- assets/js/main.js
+++ assets/js/main.js
@@ -199,43 +199,15 @@
    */
    //Get the button
 
-
-// Scroll Button
-// let mybutton = document.getElementById("btn-back-to-top");
-
-// // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function () {
-//   scrollFunction();
-// };
-
-// function scrollFunction() {
-//   if (
-//     document.body.scrollTop > 20 ||
-//     document.documentElement.scrollTop > 20
-//   ) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-// // When the user clicks on the button, scroll to the top of the document
-// mybutton.addEventListener("click", backToTop);
-
-// function backToTop() {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// }
-
-// window.addEventListener('scroll',function(){
-//   var scroll = this.document.querySelector('.scrollTop');
-//   scroll.classList.toggle("active", window.scrollY > 500)
+// Loader
+// $(window).load(function(){
+//   $('#preloader').fadeOut("slow");
 // })
 
-// function scrolltoTop(){
-//   window.scrollTo({
-//     top:0,
-//     behavior: 'smooth'
-//   })
-// }
+var loader = $document.getElementById("preloader");
+window.addEventListener("load", function(){
+  loader.style.display = "none";
+})
+
 
 })()
